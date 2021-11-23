df4c7dca254fcbc9dc9ad890ca3b8c19f36c107a
Redflame39 <vanya133719@gmail.com>
2021-11-23 13:41:31 +0300

    DependencyKeyAttribute class

---
+++ DependencyInjectionContainer/Provider/DependencyKeyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyInjectionContainer.Configuration.ImplementationData;
+
+namespace DependencyInjectionContainer.Provider
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class DependencyKeyAttribute : Attribute
+    {
+        public ImplementationNumber ImplNumber { get; }
+
+        public DependencyKeyAttribute(ImplementationNumber number)
+        {
+            this.ImplNumber = number;
+        }
+    }
+}
