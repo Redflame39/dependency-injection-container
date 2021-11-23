241d66a6ad59dbb1e07eb6ea13d39ebc965ca625
Redflame39 <vanya133719@gmail.com>
2021-11-23 13:38:33 +0300

    DependencyProvider class

---
+++ DependencyInjectionContainer/Provider/DependencyProvider.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyInjectionContainer.Configuration;
+using DependencyInjectionContainer.Configuration.ImplementationData;
+
+namespace DependencyInjectionContainer.Provider
+{
+    public class DependencyProvider
+    {
+        private readonly DependenciesConfiguration _configuration;
+        public readonly Dictionary<Type, List<SingletonContainer>> _singletons;
+
+        public DependencyProvider(DependenciesConfiguration configuration)
+        {
+            ConfigurationValidator configValidator = new ConfigurationValidator(configuration);
+            if (!configValidator.Validate())
+            {
+                throw new ArgumentException("Wrong configuration");
+            }
+
+            this._singletons = new Dictionary<Type, List<SingletonContainer>>();
+            this._configuration = configuration;
+        }
+
+        public TDependency Resolve<TDependency>(ImplementationNumber number = ImplementationNumber.Any)
+            where TDependency : class
+        {
+            return (TDependency)Resolve(typeof(TDependency), number);
+        }
+
+        public object Resolve(Type dependencyType, ImplementationNumber number = ImplementationNumber.Any)
+        {
+            object result;
+            if (this.IsIEnumerable(dependencyType))
+            {
+                result = CreateEnumerable(dependencyType.GetGenericArguments()[0]);
+            }
+            else
+            {
+                ImplementationContainer container = GetImplContainerByDependencyType(dependencyType, number);
+                Type requiredType = GetGeneratedType(dependencyType, container.ImplementationsType);
+                result = this.ResolveNonIEnumerable(requiredType, container.TimeToLive, dependencyType, container.ImplNumber);
+            }
+
+            return result;
+        }
+
+        private object ResolveNonIEnumerable(Type implType, LifeCycle ttl, Type dependencyType,
+            ImplementationNumber number)
+        {
+            if (ttl != LifeCycle.Singleton)
+            {
+                return CreateInstance(implType);
+            }
+
+            ////////
+            if (IsInSingletons(dependencyType, implType, number))
+            {
+                return this._singletons[dependencyType]
+                    .Find(singletonContainer => number.HasFlag(singletonContainer.ImplNumber)).Instance;
+            }
+
+            var result = CreateInstance(implType);
+            this.AddToSingletons(dependencyType, result, number);
+            return result;
+            ///
+        }
+
+        private ImplementationContainer GetImplContainerByDependencyType(Type dependencyType, ImplementationNumber number)
+        {
+            ImplementationContainer container;
+            if (dependencyType.IsGenericType)
+            {
+                container = GetImplementationsContainerLast(dependencyType, number);
+                container ??= GetImplementationsContainerLast(dependencyType.GetGenericTypeDefinition(), number);
+            }
+            else
+            {
+                container = GetImplementationsContainerLast(dependencyType, number);
+            }
+
+            return container;
+        }
+
+        private bool IsIEnumerable(Type dependencyType)
+        {
+            return dependencyType.GetInterfaces().Any(i => i.Name == "IEnumerable");
+        }
+
+        private object CreateInstance(Type implementationType)
+        {
+            var constructors = implementationType.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+            foreach (var constructor in constructors)
+            {
+                var constructorParams = constructor.GetParameters();
+                var generatedParams = new List<dynamic>();
+                foreach (var parameterInfo in constructorParams)
+                {
+                    dynamic parameter;
+                    if (parameterInfo.ParameterType.IsInterface)
+                    {
+                        var number = parameterInfo.GetCustomAttribute<DependencyKeyAttribute>()?.ImplNumber ?? ImplementationNumber.Any;
+                        parameter = Resolve(parameterInfo.ParameterType, number);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    generatedParams.Add(parameter);
+                }
+
+                return constructor.Invoke(generatedParams.ToArray());
+            }
+
+            throw new ArgumentException("Cannot create instance of class");
+        }
+
+        private Type GetGeneratedType(Type dependencyType, Type implementationType)
+        {
+            if (dependencyType.IsGenericType && implementationType.IsGenericTypeDefinition)
+            {
+                return implementationType.MakeGenericType(dependencyType.GetGenericArguments());
+            }
+
+            return implementationType;
+        }
+
+        private IList CreateEnumerable(Type dependencyType)
+        {
+            var implementationList = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(dependencyType));
+            var implementationsContainers = this._configuration.DependenciesDictionary[dependencyType];
+            foreach (var implementationContainer in implementationsContainers)
+            {
+                var instance = this.ResolveNonIEnumerable(implementationContainer.ImplementationsType,
+                    implementationContainer.TimeToLive, dependencyType, implementationContainer.ImplNumber);
+                implementationList.Add(instance);
+            }
+
+            return implementationList;
+        }
+
+        private ImplementationContainer GetImplementationsContainerLast(Type dependencyType, ImplementationNumber number)
+        {
+            if (this._configuration.DependenciesDictionary.ContainsKey(dependencyType))
+            {
+                return this._configuration.DependenciesDictionary[dependencyType]
+                    .FindLast(container => number.HasFlag(container.ImplNumber));
+            }
+
+            return null;
+        }
+
+        private void AddToSingletons(Type dependencyType, object implementation, ImplementationNumber number)
+        {
+            if (this._singletons.ContainsKey(dependencyType))
+            {
+                this._singletons[dependencyType].Add(new SingletonContainer(implementation, number));
+            }
+            else
+            {
+                this._singletons.Add(dependencyType, new List<SingletonContainer>()
+                {
+                    new SingletonContainer(implementation, number)
+                });
+            }
+        }
+
+        private bool IsInSingletons(Type dependencyType, Type implType, ImplementationNumber number)
+        {
+            var lst = this._singletons.ContainsKey(dependencyType) ? this._singletons[dependencyType] : null;
+            return lst?.Find(container => number.HasFlag(container.ImplNumber) && container.Instance.GetType() == implType) is not null;
+        }
+    }
+}
