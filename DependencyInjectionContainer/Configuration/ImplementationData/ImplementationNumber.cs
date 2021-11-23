293179f84c7b918ead48f99b1a5f76a7388e600f
Redflame39 <vanya133719@gmail.com>
2021-11-23 13:05:23 +0300

    ImplementationNumber emum

---
+++ DependencyInjectionContainer/Configuration/ImplementationData/ImplementationNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer.Configuration.ImplementationData
+{
+    [Flags]
+    public enum ImplementationNumber
+    {
+        None,
+        First,
+        Second,
+        Any = None | First | Second,
+    }
+}
