e9c6ddda6a16c8c80573d183e022847793e6d2bd
Redflame39 <vanya133719@gmail.com>
2021-11-23 13:06:38 +0300

    ImplementationContainer class

---
+++ DependencyInjectionContainer/Configuration/ImplementationData/ImplementationContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer.Configuration.ImplementationData
+{
+    public class ImplementationContainer
+    {
+        public Type ImplementationsType { get; }
+        public LifeCycle TimeToLive { get; }
+        public ImplementationNumber ImplNumber { get; }
+
+        public ImplementationContainer(Type implementationsType, LifeCycle timeToLive, ImplementationNumber implNumber)
+        {
+            this.ImplNumber = implNumber;
+            this.ImplementationsType = implementationsType;
+            this.TimeToLive = timeToLive;
+        }
+    }
+}
