597ac36a5f0c7cde8d1a172bd2eeba296d4c80a2
Redflame39 <vanya133719@gmail.com>
2021-11-23 13:32:26 +0300

    SingletonContainer class

---
+++ DependencyInjectionContainer/Provider/SingletonContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyInjectionContainer.Configuration.ImplementationData;
+
+namespace DependencyInjectionContainer.Provider
+{
+    public class SingletonContainer
+    {
+        public readonly ImplementationNumber ImplNumber;
+
+        public readonly object Instance;
+
+        public SingletonContainer(object instance, ImplementationNumber number)
+        {
+            this.ImplNumber = number;
+            this.Instance = instance;
+        }
+    }
+}
